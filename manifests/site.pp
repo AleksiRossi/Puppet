ef7b6de8cc81dd11991c8f4805dc26163276f7c0
root <root@puppet.local>
2016-05-01 20:46:06 +0300

    apache module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,15 @@
 node default {
 	class {'kissa':}
+<<<<<<< HEAD
+	class {'apache':}
+	class {'apache::site':}
+}
+
+=======
 }
 
 node slave1 {
 	class {'apache':}
 	class {'apache::site':}
 }
+>>>>>>> 61640b3db74db3d5d356f76a24f158e5811aad1f
--- modules/apache/manifests/site.pp
+++ modules/apache/manifests/site.pp
@@ -1,10 +1,19 @@
 class apache::site {
 	file { '/var/www/html/index.html':
 		ensure => 'absent',
+<<<<<<< HEAD
+		notify => Exec['dissite'],
+	}
+	exec { 'dissite':
+		path => [ '/bin', '/usr/bin', '/usr/sbin' ],
+		command => 'a2dissite 000-default.conif',
+		refreshonly => true,
+=======
 	}
 	exec { 'dissite':
 		path => [ '/bin', '/usr/bin', '/usr/sbin' ],
 		command => 'a2dissite 000-default.conf'
+>>>>>>> 61640b3db74db3d5d356f76a24f158e5811aad1f
 	}
 	file { '/home/aleksi/website':
 		ensure => 'directory',
@@ -14,12 +23,28 @@ class apache::site {
 	}	
  	file { '/etc/apache2/sites-available/site.conf':
 		content => template('/etc/puppet/templates/site.conf.erb'),
+<<<<<<< HEAD
+		notify => Exec['ensite'],
+=======
+>>>>>>> 61640b3db74db3d5d356f76a24f158e5811aad1f
 	}
 	exec { 'ensite':
 		path => [ '/bin', '/usr/bin', '/usr/sbin' ],
 		command => 'a2ensite site.conf',
+<<<<<<< HEAD
+		refreshonly => true,
+		notify => Service['apache2'],
 	}
 	file { '/home/aleksi/website/index.html':
 		content => 'Hello!',
+	}
+	service { 'apache2':
+		ensure => 'running',
+		enable => true
+=======
+	}
+	file { '/home/aleksi/website/index.html':
+		content => 'Hello!',
+>>>>>>> 61640b3db74db3d5d356f76a24f158e5811aad1f
 	}	
 }
