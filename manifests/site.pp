e9e653a032a8e86976108378f08b297d29958ff6
AleksiRossi <rossi.aleksi@gmail.com>
2016-05-02 20:19:55 +0300

    tweak

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,6 @@
 node default {
 	class {'kissa':}
-<<<<<<< HEAD
 	class {'apache':}
 	class {'apache::site':}
+	class {'ssh':}
 }
-
-=======
-}
-
-node slave1 {
-	class {'apache':}
-	class {'apache::site':}
-}
->>>>>>> 61640b3db74db3d5d356f76a24f158e5811aad1f
