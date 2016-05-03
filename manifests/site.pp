fb86ab9657797255e618d6d388912f1285aeecff
AleksiRossi <rossi.aleksi@gmail.com>
2016-05-03 23:29:04 +0300

    fixes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,7 @@
 node default {
 	class {'kissa':}
-<<<<<<< HEAD
 	class {'apache':
                 port => '80',
                 docroot => '/home/aleksi/website'
         }
-=======
-	class {'apache':}
-	class {'apache::site':}
-	class {'ssh':}
->>>>>>> 9038d6e78c470ae5f1a0882626a9a7ecc290ad73
 }
